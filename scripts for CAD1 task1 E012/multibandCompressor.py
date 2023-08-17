8f5a71417ec866ef263e856d2512902005bd5c05
skystriker233 <127327032+skystriker233@users.noreply.github.com>
2023-08-17 15:23:56 +0100

    Update multibandCompressor.py

--- scripts for CAD1 task1 E012/multibandCompressor.py
+++ scripts for CAD1 task1 E012/multibandCompressor.py
@@ -18,19 +18,7 @@ class MultibandCompressor:
         b, a = self.butter_bandpass(lowcut, highcut, sample_rate, order=order)
         y = lfilter(b, a, data)
         return y
-        
-    
-    #def process(self, signal, sample_rate, compressor):
-        #compressed_signal = np.zeros_like(signal)
-        #compressor = Compressor(**config.compressor)
-        #for band in self.bands:
-            #band_signal = self.butter_bandpass_filter(signal, band[0], band[1], sample_rate)
-            #band_signal , _, _ = compressor.process(band_signal)
-            # Apply compression to band_signal here
-            # Add the compressed band signal to the final signal
-            #compressed_signal += band_signal
-        #return compressed_signal 
-        
+               
     def process(self, signal, sample_rate, compressor):
         #compressed_signal = np.zeros_like(signal)
         compressed_signal = np.copy(signal)
@@ -38,7 +26,6 @@ class MultibandCompressor:
             band_signal = self.butter_bandpass_filter(signal, band[0], band[1], sample_rate)
             compressed_signal -= band_signal
             band_signal_new, _, _ = compressor.process(band_signal)
-            # Apply compression to band_signal here
             # Add the compressed band signal to the final signal
             compressed_signal += band_signal_new
         return compressed_signal
